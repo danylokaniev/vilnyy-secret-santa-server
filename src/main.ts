8a003d00c393c6e2d7b5b9daa86fe4102001b1a4
Danylo <danylo@code-care.pro>
2022-12-08 10:31:37 +0200

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const authOptions = new DocumentBuilder()
+    .setTitle('Villny Secret Santa API')
+    .setDescription('# About\nBasic description\n')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'secret_token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, authOptions);
+
+  await SwaggerModule.setup('/', app, document);
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
