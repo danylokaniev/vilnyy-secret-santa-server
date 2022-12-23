7d1253c7c3550dc04106f1b03708cfc11466fbc4
Danylo <danylo@code-care.pro>
2022-12-23 13:17:28 +0200

    Add display ngnix.conf file

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d vilnyyapi-env.eba-pnaebryc.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email dkanevb@gmail.com 
+cat /etc/nginx/nginx.conf && sudo certbot -n -d vilnyyapi-env.eba-pnaebryc.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email dkanevb@gmail.com 
