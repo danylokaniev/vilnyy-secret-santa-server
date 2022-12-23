4b39226dd409ae2de3818457d832f57823922450
Danylo <danylo@code-care.pro>
2022-12-23 15:42:19 +0200

    Remove nginx restart

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-echo "HELLO 0" && sudo ls /etc/nginx/conf.d && echo "HELLO" && sudo ls /etc/nginx/conf.d/  && echo "HELLO I AM THERE" && sudo ls /etc/nginx/conf.d/  && echo "HELLO my file" && sudo cat /etc/nginx/nginx.conf && sudo certbot -n -d vilnyyapi-env.eba-pnaebryc.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email dkanevb@gmail.com  && sudo systemctl restart nginx
+echo "HELLO 0" && sudo ls /etc/nginx/conf.d && echo "HELLO" && sudo ls /etc/nginx/conf.d/  && echo "HELLO I AM THERE" && sudo ls /etc/nginx/conf.d/  && echo "HELLO my file" && sudo cat /etc/nginx/nginx.conf && sudo certbot -n -d vilnyyapi-env.eba-pnaebryc.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email dkanevb@gmail.com  
