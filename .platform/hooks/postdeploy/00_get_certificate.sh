d12f76e91b6f9727ae9b371274dbb85ba9105cae
Danylo <danylo@code-care.pro>
2022-12-23 13:34:19 +0200

    Add ls commands to the script'

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-echo "HELLO I AM THERE" && sudo cat /etc/nginx/nginx.conf && sudo certbot -n -d vilnyyapi-env.eba-pnaebryc.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email dkanevb@gmail.com 
+sudo ls /etc/nginx/conf.d/  && echo "HELLO I AM THERE" && sudo ls /etc/nginx/conf.d/  && sudo cat /etc/nginx/nginx.conf && sudo certbot -n -d vilnyyapi-env.eba-pnaebryc.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email dkanevb@gmail.com 
