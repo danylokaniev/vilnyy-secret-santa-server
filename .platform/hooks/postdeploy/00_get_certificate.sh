f2e6a9808df0526bab804be2f1709aa485fb5f69
Danylo <danylo@code-care.pro>
2022-12-23 13:49:01 +0200

    Modify 00 script

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo cat /etc/nginx/conf.d/custom_nginx.conf && echo "HELLO" && sudo ls /etc/nginx/conf.d/  && echo "HELLO I AM THERE" && sudo ls /etc/nginx/conf.d/  && echo "HELLO my file" && sudo cat /etc/nginx/nginx.conf && sudo certbot -n -d vilnyyapi-env.eba-pnaebryc.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email dkanevb@gmail.com 
+echo "HELLO 0" && sudo ls /etc/nginx/conf.d && echo "server_names_hash_bucket_size  128;" > /etc/nginx/conf.d/custom_nginx.conf && echo "HELLO" && sudo ls /etc/nginx/conf.d/  && echo "HELLO I AM THERE" && sudo ls /etc/nginx/conf.d/  && echo "HELLO my file" && sudo cat /etc/nginx/nginx.conf && sudo certbot -n -d vilnyyapi-env.eba-pnaebryc.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email dkanevb@gmail.com 
